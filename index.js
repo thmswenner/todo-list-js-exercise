3e395cdfad9296da5f160769d92f113ae3b6dc29
Thomas Wenner <thmswenner@gmail.com>
2019-04-30 17:12:53 +0000

    Add description to output

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function newTask(title, description) {
     complete: false,
 
     logState: function() {
-      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+      console.log(`${this.title}, ${this.description} has${this.complete ? " " : " not "}been completed`);
     },
 
     markCompleted: function() {
